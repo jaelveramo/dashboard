0b530b6a550b50e657e670cb066c2cb29d6da5bd
unknown <jaelveramo@yahoo.es>
2023-02-07 21:58:37 -0500

    dashboard-20230207

---
+++ assets/js/update.js
@@ -0,0 +1,21 @@
+let runScripts = () => {
+    let chart_bars = document.getElementById("chart-bars");
+    //Esto no se debe hacer, solo para seguimiento
+    //console.log(chart_bars);
+    let icons = document.getElementsByTagName("i");
+    //console.log(icons);
+    let cards = document.getElementsByClassName("card");
+    //console.log(cards);
+};
+
+let modifyText = () => {
+    let listOfP = document.getElementsByTagName("p");
+    let elementP = listOfP[3];
+    elementP.innerHTML = "Dinero actual";
+    let listOfH4 = document.getElementsByTagName("h4");
+    let elementH4 = listOfH4[0];
+    elementH4.innerHTML = "$301k";
+}
+
+runScripts();
+modifyText();
